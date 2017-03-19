976acf0e95c2ec14b4e4a3e72035df4bae2e6e3b
blurium <abalkan@ucla.edu>
2017-03-18 23:10:15 -0700

    blah

--- data2bis.m
+++ data2bis.m
@@ -1,3 +1,4 @@
+close all;
 delta=[0.8036 0.8058 0.8900 0.9249 0.9493 0.9575 0.9695 0.9728 0.9754 0.9774 0.9790];
 
 lb=[0.4751 0.5204 0.5370 0.5392 0.5458 0.5458 0.5469 0.5469 0.5469 0.5469 0.5469];
@@ -14,7 +15,7 @@ grid on
 plot(points,lb,'b','LineWidth',2)
 plot(points,ub,'r','LineWidth',2)
 plot(points,a,'--k','LineWidth',1)
-plot(points,b,'*k','LineWidth',1)
+plot(points,b,'-.k','LineWidth',1)
 ylim([0 1]);
 xlim([0 520]);
 
@@ -22,6 +23,6 @@ xlim([0 520]);
 
 xlabel('Number of samples (N)','FontWeight','bold')
 
-legend('Lower Bound','Upper bound','\rho','\rho / \surd 2','Interpreter','latex')
+legend('Lower Bound','Upper bound','\rho','\rho / \surd n','Interpreter','latex')
 %legend('\sqrt{n}','Interpreter','latex')
 %ylabel('Bounds','FontWeight','bold')
