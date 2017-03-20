2a81c3e1838edee0adaf458c64c27050c4bf72d6
blurium <abalkan@ucla.edu>
2017-03-20 14:29:12 -0700

    all little changes

--- data2bis.m
+++ data2bis.m
@@ -25,7 +25,7 @@ xlabel('Number of samples (N)','FontWeight','bold')
 
 
 
-h_legend = legend('\gamma*(\omega_N)/\surd n','\gamma*(\omega_N)/\delta','\rho','\rho / \surd n','Interpreter','latex','FontSize')
+h_legend = legend('Lower bound','Upper bound','\rho','\rho / \surd n','Interpreter','latex','FontSize')
 set(h_legend,'FontSize',20);
 %legend('\sqrt{n}','Interpreter','latex')
 %ylabel('Bounds','FontWeight','bold')
